9597917b03c76d227dd5e7811efad767cd4b91ff
Jeison <herrerajeison1905@gmail.com>
2025-04-02 23:29:43 -0500

    V6: Barra de navegacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,12 +43,41 @@
 </head>
 <body>
 
-    <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="{{ route('tareas.index') }}">Projects-JMC</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('tareas.index') }}">Tareas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('proyectos.index') }}">Proyectos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('dashboard.index') }}">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('perfil.index') }}">Perfil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('usuarios.index') }}">Usuarios</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+            <form method="POST" action="{{ route('tareas.index') }}">
+                @csrf
+                <button type="submit" class="btn btn-outline-danger">Cerrar Sesión</button>
+            </form>
+        </li>
+    </ul>
+</div>
+                
+            </div>
         </div>
     </nav>
 
